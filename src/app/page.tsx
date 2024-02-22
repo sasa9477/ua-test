1270940d130feae2526c4ae334b2c930b092448a
sasa9477 <sasa9477@outlook.com>
2024-02-22 16:45:11 +0900

    add some devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@
 import styles from "./page.module.css";
 import { isMobile as isMobileCheck } from "@/utils/isMobile";
 import { isMobile as isMobileModule, isMobileResult } from "is-mobile";
-import { isMobile as isMobileReactDeviceDetect } from "react-device-detect";
+import {
+  isMobile as isMobileReactDeviceDetect,
+  isIPad13,
+  isTablet,
+  deviceDetect,
+  deviceType,
+} from "react-device-detect";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -33,6 +39,10 @@ export default function Home() {
       <h1 className={styles.title}>ua-test</h1>
       <h2 className={styles.sub_title}>react-device-detect</h2>
       <p className={styles.is_mobile}>isMobile {isMobileReact ? "true" : "false"}</p>
+      <p className={styles.is_mobile}>isTablet {isTablet ? "true" : "false"}</p>
+      <p className={styles.is_mobile}>isIPad {isIPad13 ? "true" : "false"}</p>
+      <p className={styles.is_mobile}>deviceType {deviceType}</p>
+      <pre className={styles.detail}>{JSON.stringify(deviceDetect(undefined), null, 2)}</pre>
 
       <h2 className={styles.sub_title}>utils</h2>
       <p className={styles.is_mobile}>isMobile {isMobile ? "true" : "false"}</p>
