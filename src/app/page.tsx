f2de38c65ad190dfa3f0f7330b1db798b29c691b
sasa9477 <sasa9477@outlook.com>
2024-02-22 11:27:08 +0900

    add mobile module interim-results

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,28 @@
 "use client";
 
-import Image from "next/image";
 import styles from "./page.module.css";
-import { isMobile } from "@/utils/isMobile";
-import isMobileModule from "is-mobile";
+import { isMobile as isMobileCheck } from "@/utils/isMobile";
+import { isMobile as isMobileModule, isMobileResult } from "is-mobile";
+import { useEffect, useMemo, useState } from "react";
 
 export default function Home() {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+  const [isMobileResult, setIsMobileResult] = useState<isMobileResult>({ result: false, detail: null });
+
+  useEffect(() => {
+    setIsMobile(isMobileCheck());
+  }, [setIsMobile]);
+
+  useEffect(() => {
+    setIsMobileResult(isMobileModule());
+  }, [setIsMobileResult]);
+
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>ua-test</h1>
-      <p className={styles.is_mobile}>isMobile {isMobile() ? "true" : "false"}</p>
-      <p className={styles.is_mobile}>isMobile {isMobileModule() ? "true" : "false"}</p>
+      <p className={styles.is_mobile}>isMobile {isMobile ? "true" : "false"}</p>
+      <p className={styles.is_mobile}>(npm module) isMobile {isMobileResult.result ? "true" : "false"}</p>
+      {isMobileResult?.detail && <pre className={styles.detail}>{JSON.stringify(isMobileResult.detail, null, 2)}</pre>}
     </main>
   );
 }
